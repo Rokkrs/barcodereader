40fafa499cd8006e8b44b58a8a7e87528e05f5c3
Rokkrs <oscar.cuadra.navarro@gmail.com>
2024-11-16 00:31:12 -0300

    init

---
+++ barcodeReader/barcodeReader/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  barcodeReader
+//
+//  Created by Oscar Hooman on 11/16/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ barcodeReader/barcodeReaderTests/barcodeReaderTests.swift
@@ -0,0 +1,17 @@
+//
+//  barcodeReaderTests.swift
+//  barcodeReaderTests
+//
+//  Created by Oscar Hooman on 11/16/24.
+//
+
+import Testing
+@testable import barcodeReader
+
+struct barcodeReaderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
